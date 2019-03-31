fb524a2b0f65d32eec25b2a74b5f064a4aef6f68
xiongm <xiongm@gmail.com>
2019-03-31 06:47:50 +0000

    r #0001: update config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+SUBSCRIPTIONS=['twitter', 'facebook', 'google', 'travel', 'art', 'music', 'photography', 'love', 'fashion', 'food']
+CONSUMER_KEY = 'consumer_key'
+CONSUMER_SECRET =  'consumer_secrete'
+ACCESS_TOKEN = 'access_token'
+ACCESS_TOKEN_SECRET = 'access_token_secrete'
+PLOT_STYLE="fivethirtyeight"
+PLOT_Y_LABEL="sentiment"
+PLOT_X_LABEL="time sequence"
+CHART_WINDOW_SIZE=100
+
