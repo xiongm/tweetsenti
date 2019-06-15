8090686b812ec0d19d88ef5a1a70b8913cf50771
xiongm <xiongm@gmail.com>
2019-06-15 00:22:13 -0400

    Update docker

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 SUBSCRIPTIONS=['twitter', 'facebook', 'google', 'travel', 'art', 'music', 'photography', 'love', 'fashion', 'food']
-CONSUMER_KEY = 'consumer_key'
-CONSUMER_SECRET =  'consumer_secrete'
-ACCESS_TOKEN = 'access_token'
-ACCESS_TOKEN_SECRET = 'access_token_secrete'
+CONSUMER_KEY = 'Io6JusF5ajIPFk5qSFrIG47cE'
+CONSUMER_SECRET =  'jSa6aaF6ubRbHsI6i06NQeTDzFXaquNZkuDl1UURzqpDFIbWg3'
+ACCESS_TOKEN = '4266821-gyQHDvCArlEMenD4E2Ai9ry5WPTMfyOcAhmF4iPHuO'
+ACCESS_TOKEN_SECRET = 'yRTmfnJHREpxDy3VWHbsLn3jFK6wYHeqnJfQqoz3VxPZF'
 PLOT_STYLE="fivethirtyeight"
 PLOT_Y_LABEL="sentiment"
 PLOT_X_LABEL="time sequence"
